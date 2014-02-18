66db03a4e9f71e9f15afa377760b6d7f01af1f42
Liam <mail@liam-conroy.com>
2014-02-18 18:34:52 +0900

    trying out fragments

---
+++ app/src/main/java/com/example/myfirstapplication/HeadlineFragment.java
@@ -0,0 +1,20 @@
+package com.example.myfirstapplication;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by liam on 2/18/14.
+ */
+
+public class HeadlineFragment extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.news_headlines, container, false);
+    }
+}
