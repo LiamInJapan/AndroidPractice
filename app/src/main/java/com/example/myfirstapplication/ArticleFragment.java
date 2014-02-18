4eecb143bda6cb6ea62090d632f0b716534f460b
Alessandro Gatti <a.gatti@frob.it>
2014-02-18 18:01:42 +0800

    Don't recurse layouts inflation.

--- app/src/main/java/com/example/myfirstapplication/ArticleFragment.java
+++ app/src/main/java/com/example/myfirstapplication/ArticleFragment.java
@@ -14,6 +14,6 @@ public class ArticleFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.news_articles, container, false);
+        return inflater.inflate(R.layout.fragment_articles, container, false);
     }
 }
